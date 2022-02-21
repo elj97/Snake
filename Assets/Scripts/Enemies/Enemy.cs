6d942de3001d9fbd0ed2b9c6a6207a28eec209f2
PamWA <pambalam6782@gmail.com>
2022-02-21 20:46:41 +0800

    Work on Enemy Spawner and Enemy - Using Object Pooling and NavMesh
    Currently there are errors with the navmesh spawning + pooling not being set right + scriptable object is not applying values correctly

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -1,15 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class Enemy : MonoBehaviour
 {
+	public NavMeshTriangulation Triangulation { get; set; }
+	public NavMeshAgent Agent { get { return m_Agent; } }
+
 	[SerializeField] EnemyScriptableObject m_EnemyScriptableObject;
-	[SerializeField] EnemyType m_EnemyTypeTest;
+
+	private void Start()
+	{
+		m_Agent = GetComponent<NavMeshAgent>();
+	}
 
 	private void OnEnable()
 	{
 		SetupAgentFromConfiguration();
+		m_Initialized = true;
+	}
+
+	private void OnDisable()
+	{
+		m_Initialized = false;
 	}
 
 	private void Update()
@@ -24,19 +39,23 @@ public class Enemy : MonoBehaviour
 		{
 			m_FrameCounter = 1;
 		}
-
-		// Movement
-		if (m_FrameCounter % m_MovementUpdateTime == 0)
+		if (m_Initialized)
 		{
-			Movement();
+			// Movement
+			if (m_FrameCounter % m_MovementUpdateTime == 0)
+			{
+				Movement();
+			}
 		}
 	}
 
 	private void SetupAgentFromConfiguration()
 	{
-		m_Self = m_EnemyScriptableObject.m_Type;
-		m_Movement = m_EnemyScriptableObject.m_Movement;
-		m_Speed = m_EnemyScriptableObject.m_MovementSpeed;
+		//m_Self = m_EnemyScriptableObject.m_Type;
+		m_Self = EnemyType.AOE;
+		//m_Movement = m_EnemyScriptableObject.m_Movement;
+		m_Movement = EnemyMovementType.Erratic;
+		//m_Speed = m_EnemyScriptableObject.m_MovementSpeed;
 
 		SetupBehaivour();
 	}
@@ -56,56 +75,61 @@ public class Enemy : MonoBehaviour
 		{
 			// Ranged Behaivour
 		}
-
-		switch (m_Movement)
-		{
-			case EnemyMovementType.CirclePlayer:
-
-				break;
-			case EnemyMovementType.Erratic:
-
-				break;
-			case EnemyMovementType.FollowPlayer:
-
-				break;
-			case EnemyMovementType.RamPlayer:
-
-				break;
-			case EnemyMovementType.Spin:
-
-				break;
-		}
 	}
 
 	private void Movement()
 	{
-		if (m_Movement == EnemyMovementType.Erratic)
-		{
-			m_Target = ErraticTarget();
-		}
-
-		Vector3 positionTarget = m_Target;
+		Vector3 positionTarget = Target();
 		Vector3 moveDirection = positionTarget - transform.position;
-		transform.position += moveDirection * m_Speed * Time.deltaTime;
+		//transform.position += moveDirection * m_Speed * Time.deltaTime;
+		m_Agent.destination = positionTarget;
 
 		float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
 		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 	}
 
-	private Vector3 ErraticTarget()
+	private Vector3 Target()
 	{
-		// Use navmesh to determine possible target placements
+		// switch statement
+		switch (m_Movement)
+		{
+			case EnemyMovementType.CirclePlayer:
+				return Vector3.zero;
+			case EnemyMovementType.Erratic:
+				int vertexIndex = Random.Range(0, Triangulation.vertices.Length);
+				NavMeshHit hit;
+				if (NavMesh.SamplePosition(Triangulation.vertices[vertexIndex], out hit, 2f, -1))
+				{
+					return hit.position;
+				}
+				else
+				{
+					// If no target was found set target to zero
+					Debug.LogWarning("Couldn't find new target on NavMeshAgent");
+					return Vector3.zero;
+				}
+			case EnemyMovementType.FollowPlayer:
+				return Vector3.zero;
+			case EnemyMovementType.RamPlayer:
+				return Vector3.zero;
+			case EnemyMovementType.Spin:
+				return Vector3.zero;
+		}
+		Debug.LogError("Could not find new Target");
 		return Vector3.zero;
 	}
 
 	// Do these need to be organised?
+	private bool m_Initialized;
+
+	private NavMeshAgent m_Agent;
+
 	private float m_Speed;
 	private EnemyType m_Self;
 	private EnemyMovementType m_Movement;
 
-	// How many frames per update to where the enemy should move
-	private int m_MovementUpdateTime = 1;
-	private Vector3 m_Target;
+	// How many frames per update to where the enemy should move, should probably be in seconds instead? idk
+	private int m_MovementUpdateTime = 300;
 
 	private short m_FrameCounter;
 }
--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -2,29 +2,83 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Pool;
 
 public class EnemySpawner : MonoBehaviour
 {
-	[SerializeField] NavMeshAgent m_Agent;
 	[SerializeField] int m_EnemyAmount = 5;
 	[SerializeField] float m_SpawnDelay = 1f;
-	[SerializeField] List<Enemy> EnemyPrefabs = new List<Enemy>();
+	[SerializeField] Enemy m_EnemyPrefabTest;
 
 	private void Awake()
 	{
 		// Is expensive and in the spawner itself should only be called once and then used for all enemy spawns 
-		NavMeshTriangulation triangulation = NavMesh.CalculateTriangulation();
+		NavMeshTriangulation m_Triangulation = NavMesh.CalculateTriangulation();
+	}
 
-		int vertexIndex = Random.Range(0, triangulation.vertices.Length);
+	private void Start()
+	{
+		m_Pool = new ObjectPool<Enemy>(CreatePooledItem, OnTakeFromPool, OnReturnedToPool, OnDestroyPoolObject, false, 10, 20);
 
-		NavMeshHit hit;
-		if (NavMesh.SamplePosition(triangulation.vertices[vertexIndex], out hit, 2f, -1))
+		//InvokeRepeating(nameof(Spawn), 0.2f, 0.2f);
+
+		for (var i = 0; i < m_EnemyAmount; i++)
 		{
-			m_Agent.Warp(hit.position);
-		}
-		else
-		{
-			Debug.LogError("Couldn't place on NavMeshAgent");
+			var enemy = m_Pool.Get();
+			enemy.transform.position = Vector3.zero;
 		}
 	}
+
+	#region Pooling
+	private Enemy CreatePooledItem()
+	{
+		return Instantiate(m_EnemyPrefabTest);
+	}
+	private void OnReturnedToPool(Enemy enemy)
+	{
+		enemy.gameObject.SetActive(false);
+		// Can also have de-initialisation here
+	}
+	private void OnTakeFromPool(Enemy enemy)
+	{
+		enemy.gameObject.SetActive(true);
+		/// Initialization
+		enemy.Triangulation = m_Triangulation;
+		// Spawn Position
+		int vertexIndex = Random.Range(0, m_Triangulation.vertices.Length);
+		NavMeshHit hit;
+		if (NavMesh.SamplePosition(m_Triangulation.vertices[vertexIndex], out hit, 2f, -1))
+		{
+			enemy.Agent.Warp(hit.position);
+		}
+		/*else
+		{
+			// If no spawn was found re-add the enemy to the pool and send an error
+			Debug.LogError("Couldn't find placement on NavMeshAgent");
+			m_Pool.Release(enemy);
+		}*/
+	}
+	private void OnDestroyPoolObject(Enemy enemy)
+	{
+		Destroy(enemy.gameObject);
+	}
+	#endregion
+
+	private void Spawn()
+	{
+		for (var i = 0; i < m_EnemyAmount; i++)
+		{
+			var enemy = m_Pool.Get();
+			enemy.transform.position = Vector3.zero;
+		}
+	}
+
+	private void KillEnemy(Enemy enemy)
+	{
+		m_Pool.Release(enemy);
+	}
+
+	private ObjectPool<Enemy> m_Pool;
+
+	private NavMeshTriangulation m_Triangulation;
 }
